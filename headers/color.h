22fd5ae5c427c7768ce9289a03f929a4985b75d6
Adandi <rwindowed@gmail.com>
2025-01-30 18:09:42 +0200

    Create color.h

---
+++ headers/color.h
@@ -0,0 +1,24 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define COLOR_BLACK      "30"
+#define COLOR_BLUE       "34"
+#define COLOR_GREEN      "32"
+#define COLOR_CYAN       "36"
+#define COLOR_RED        "31"
+#define COLOR_MAGENTA    "35"
+#define COLOR_YELLOW     "33"
+#define COLOR_WHITE      "37"
+
+#define COLOR_DARKGRAY   "90"
+#define COLOR_LIGHTBLUE  "94"
+#define COLOR_LIGHTGREEN "92"
+#define COLOR_LIGHTCYAN  "96"
+#define COLOR_LIGHTRED   "91"
+#define COLOR_LIGHTMAGENTA "95"
+#define COLOR_LIGHTYELLOW "93"
+#define COLOR_LIGHTWHITE  "97"
+
+void set_color(int color);
+
+#endif
