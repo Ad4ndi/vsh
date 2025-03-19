a1bf6b7f9c6ac0b566ec57a8bac4c19c429ce0c2
Adandi <rwindowed@gmail.com>
2025-03-19 15:42:19 +0200

    Update prompt.c

--- src/prompt.c
+++ src/prompt.c
@@ -5,20 +5,17 @@
 #include "../headers/prompt.h"
 #include "../headers/color.h"
 
-void parse() {
+void parse
+(void) {
     char *temp_prompt = prompt, buffer[256];
-    for (int i = 0; temp_prompt[i] != '\0'; i++) {
+    for (int i = 0; temp_prompt[i]; i++) {
         if (temp_prompt[i] == '\\') {
             i++;
             switch (temp_prompt[i]) {
                 case 'h': gethostname(buffer, sizeof(buffer)); printf("%s", buffer); break;
-                case 'u': {
-                    char *user = getenv("USER");
-                    if (user) printf("%s", user);
-                    break;
-                }
-                case 'w': if (getcwd(buffer, sizeof(buffer)) != NULL) printf("%s", buffer); break;
-                case 'c': if (temp_prompt[++i] == '(') set_color(atoi(&temp_prompt[i+1])); while (temp_prompt[i] != ')' && temp_prompt[i] != '\0') i++; break;
+                case 'u': printf("%s", getenv("USER")); break;
+                case 'w': if (getcwd(buffer, sizeof(buffer))) printf("%s", buffer); break;
+                case 'c': if (temp_prompt[++i] == '(') set_color(atoi(&temp_prompt[i+1])); while (temp_prompt[i] != ')' && temp_prompt[i]) i++; break;
                 default: putchar('\\'); putchar(temp_prompt[i]);
             }
         } else {
