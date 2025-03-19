52fc3250276e94e234aa2151834293f60f0fcab1
Adandi <rwindowed@gmail.com>
2025-03-19 15:42:40 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,25 +15,22 @@ extern char path[];
 #define MAX_INPUT_LENGTH 256
 #define MAX_ARGV_LENGTH 64
 
-int main() {
+int main
+(void) {
     char input[MAX_INPUT_LENGTH], *argv[MAX_ARGV_LENGTH], libpath[MAX_INPUT_LENGTH];
     void *handle;
     int i;
 
-    if (setenv("PATH", path, 1) == -1) {
-        perror("vsh");
-        return 1;
-    }
+    if (setenv("PATH", path, 1) == -1) return 1;
 
     while (1) {
         parse();
-        if (!fgets(input, sizeof(input), stdin)) { perror("vsh"); break; }
+        if (!fgets(input, sizeof(input), stdin)) break;
         input[strcspn(input, "\n")] = '\0';
-        if (*input == '\0') continue;
-        if (!strcmp(input, "exit")) break;
+        if (!*input || !strcmp(input, "exit")) break;
 
         i = 0;
-        for (char *token = strtok(input, " "); token && i < MAX_ARGV_LENGTH - 1; token = strtok(NULL, " "))
+        for (char *token = strtok(input, " "); token && i < MAX_ARGV_LENGTH - 1; token = strtok(NULL, " ")) 
             argv[i++] = token;
         argv[i] = NULL;
 
@@ -43,25 +40,19 @@ int main() {
                 fprintf(stderr, "vsh: %s: %s\n", libpath, dlerror());
                 continue;
             }
+            
             void (*func)(const char *) = (void (*)(const char *)) dlsym(handle, argv[1]);
-            if (!func) {
-                fprintf(stderr, "vsh: %s: %s\n", argv[1], dlerror());
-                dlclose(handle);
-                continue;
-            }
-            func(argv[2] ? argv[2] : "");
+            if (func) func(argv[2] ? argv[2] : "");
+            else fprintf(stderr, "vsh: %s: %s\n", argv[1], dlerror());
             dlclose(handle);
         } else {
             pid_t pid = fork();
-            if (pid == -1) { perror("vsh"); continue; }
-            if (!pid) {
-                if (execvp(argv[0], argv) == -1) {
-                    perror("vsh");
-                    exit(1);
-                }
-            } else if (waitpid(pid, NULL, 0) == -1) {
+            if (pid == 0) {
+                execvp(argv[0], argv);
                 perror("vsh");
+                exit(1);
             }
+            if (pid > 0) waitpid(pid, NULL, 0);
         }
     }
     return 0;
